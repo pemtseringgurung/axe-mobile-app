527e14f573727b0deeb9b5a636685cb76f669e10
pemtseringgurung <pemgurung541@gmail.com>
2025-12-06 21:16:47 -0500

    Initial Commit

---
+++ axe-mobile-app/axe_mobile_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  axe_mobile_appApp.swift
+//  axe-mobile-app
+//
+//  Created by Pem Tsering Gurung on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct axe_mobile_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
